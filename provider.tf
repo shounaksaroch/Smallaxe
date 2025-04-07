381c9ecdaf0f585bfcba6ba8224b3fe009434804
shounaksaroch <163114670+shounaksaroch@users.noreply.github.com>
2025-04-07 14:24:28 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,6 +10,6 @@ terraform {
 
 # Configure the Azure Active Directory Provider
 provider "azuread" {
-  tenant_id = var.entra_tenant_id
+  tenant_id =  ${{ secrets.AZ_TEST_TENANT_ID }}
   #tenant_id = "570c8a00-bc2e-466a-918b-a6e4e6326c02"
 }
